35c3b0c094b80f5430c5ef4d634165d2e4e5aa71
Jonathan Ballerano <jballer@jballer.com>
2013-11-05 23:26:53 -0500

    Home Page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.Play;
 import play.mvc.*;
 
 import java.util.*;
@@ -9,8 +10,19 @@ import models.*;
 
 public class Application extends Controller {
 
+	@Before
+	static void addDefaults() {
+		renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+		renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+	}
+
     public static void index() {
-        render();
+		
+		// Get and display the list of posts
+		Post frontPost = Post.find("order by postedAt desc").first();
+		List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
+		
+        render(frontPost, olderPosts);
     }
 
 }
