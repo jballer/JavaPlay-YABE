38ca546f9e86d59deadce0c6dc605a6e38c5bf6f
Jonathan Ballerano <jballer@jballer.com>
2013-11-09 10:17:50 -0500

    Securing admin pages; before customizing CRUD layout

---
+++ app/controllers/Security.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import models.*;
+
+public class Security extends Secure.Security {
+
+	static boolean authenticate(String username, String password) {
+		return User.connect(username, password) != null;
+	}
+	
+	static void onDisconnected() {
+		Application.index();
+	}
+	
+	static void onAuthenticated() {
+		Application.index();
+	}
+	
+	static boolean check(String profile) {
+		if("admin".equals(profile)) {
+			User user = User.find("byEmail", connected()).first();
+			return user.isAdmin;
+		}
+		
+		return false;
+	}
+}
