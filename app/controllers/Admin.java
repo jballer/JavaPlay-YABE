ffe0581fd154ed6b8e1517fbcfb767ccf6e23682
Jonathan Ballerano <jballer@jballer.com>
2013-11-09 15:19:59 -0500

    Custom editor area

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -20,6 +20,37 @@ public class Admin extends Controller {
 	}
 	
 	public static void index() {
+		String user = Security.connected();
+		List<Post> posts = Post.find("author.email", user).fetch();
+		render(posts);
+	}
+	
+	public static void form() {
 		render();
+	}
+	
+	public static void save(String title, String content, String tags) {
+		
+		// Make the post
+		User author = User.find("byEmail", Security.connected()).first();
+		Post post = new Post(author, title, content);
+		
+		// Make the tags
+		for(String tag : tags.split("\\s+")) {
+			if(tag.trim().length() > 0) {
+				post.tags.add(Tag.findOrCreateByName(tag));
+			}
+		}
+		
+		// Validate the post
+		validation.valid(post);
+		if(validation.hasErrors()) {
+			render("@form", post);
+		}
+		
+		// Save
+		post.save();
+		index();
+		
 	}
 }
